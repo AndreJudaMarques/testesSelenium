e4d0a77dd1f4075b41af4822f7bf2f1cceb60976
André Judá <judatrainer@gmail.com>
2024-06-07 12:00:35 -0300

    xpath aula

--- encontrarPorXpath.py
+++ encontrarPorXpath.py
@@ -19,13 +19,34 @@ def iniciar_driver():
 driver = iniciar_driver()
 driver.get('https://cursoautomacao.netlify.app/')
 
-#encontrar elementos por Texto web
+#encontrar elementos por Xpath
 
 zona = driver.find_element(By.XPATH,"//*[text()='ZONA DE TESTES']")
 
 if zona is not None:
       print('achei a zona')
 
+# //* = todos
+# //*[h4]
+# //*[contains(text(), 'Exemplo')]
+# //*[contains(text(), 'Dropdown') or contains(text(), 'estilizados')]
+# //*[starts-with(text(), 'exemplo')]
+# //*[starts-with(@class, 'btn')]
+# //*[text()='Exemplo Checkbox']
+# //h4[text()='Exemplo Checkbox'] MAIS ESPECIFICO
+# //button[@aria-label='Toggle navigation'] MAIS ESPECIFICO
+# //section[@class='jumbotron']
+# //div[@class='form-check']
+# //div[@id='select-class-example']//fieldset/h4 ENCONTRAR 1 ELEMENTE DENTRO DE OUTROS
+# //thead//tr//th[3] ENCONTRAR 1 ELEMENTE DENTRO DE OUTROS
+
+umatag = driver.find_element(By.XPATH, "//thead//tr//th[3]")
+variastag = driver.find_elements(By.XPATH,'//div/fieldset')
+
+if umatag is not None:
+      print('achei 1 tag')
+if variastag is not None:
+      print('achei as tags')
 
 input(' Enter para sair...')
 driver.close()
